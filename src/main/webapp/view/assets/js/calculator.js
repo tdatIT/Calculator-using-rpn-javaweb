31ff6afffbcef63f6adecb77cedc6d90add5f1bf
Le Hai <hainaruto01@gmail.com>
2022-10-27 21:46:32 +0700

    Hoàn thành chức năng giao diện

---
+++ src/main/webapp/view/assets/js/calculator.js
@@ -0,0 +1,92 @@
+//selector
+const input = document.querySelector(".input");
+const result = document.querySelector(".result");
+const keys = document.querySelectorAll("button");
+const output = document.querySelector(".output_postfix");
+
+//eventlistener
+keys.forEach(key => {
+    key.addEventListener("click", cal);
+});
+
+function cal() {
+    let buttonText = this.innerText;
+    if (buttonText === "AC") {
+        input.innerText = "";
+        output.innerText = "";
+        result.innerText = "0";
+        result.style.animation = "";
+        input.style.animation = "";
+        return;
+    }
+    if (buttonText === "DEL") {
+        input.textContent = input.textContent.substr(0, input.textContent.length - 1);
+        output.textContent = output.textContent.substr(0, output.textContent.length - 1);
+        return;
+    }
+    if (buttonText === "=") {
+        //result.innerText = etext(output.innerText);
+        result.style.animation = "big 0.5s ease-in-out";
+        input.style.animation = "big 0.5s ease-in-out";
+        output.style.animation = "big 0.5s ease-in-out";
+        result.style.animationFillMode = "forwards";
+        input.style.animationFillMode = "forwards";
+        output.style.animationFillMode = "forwards";
+
+    } else {
+        input.textContent += buttonText;
+        return;
+    }
+
+}
+
+function getDataToServer() {
+    let infix_str = $('.input').text();
+    $.post("calculate", {infix: infix_str}, function (response) {
+        const result_arr = response.split('/');
+        if (result_arr[0] === 'null') {
+            $(".result").text("Expression Error Syntax")
+            $(".output_postfix").text("");
+        } else {
+            $(".result").text(result_arr[0]);
+            $(".output_postfix").text(result_arr[1]);
+        }
+
+    })
+}
+
+function clearDisplay() {
+    $('.input').text("");
+}
+
+function backSpaceDisplay() {
+    let str = $('.input').text();
+    $('.input').text(str.slice(0, -1));
+}
+
+$(document).ready(function () {
+    $('#send').click(function (e) {
+        getDataToServer();
+    })
+})
+$(document).keydown(function (e) {
+    let display_str = $('.input').text();
+    switch (e.keyCode) {
+        case 8:
+            backSpaceDisplay()
+            break;
+        case 13:
+            getDataToServer()
+            break;
+        case 46:
+            clearDisplay()
+            break;
+        default:
+            if ((e.keyCode >= 48 && e.keyCode <= 57)
+                || (e.keyCode >= 96 && e.keyCode <= 111)
+                || (e.keyCode >= 187 && e.keyCode <= 191)) {
+                $('.input').text($('.input').text() + e.key);
+            }
+            break;
+    }
+})
