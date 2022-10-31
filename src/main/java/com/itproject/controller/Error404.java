194218ac48010d089de0098ee3d33f5f5433dbed
tdatIT <tdat.it2k2@gmail.com>
2022-10-31 13:46:47 +0700

    Cập nhật thiết kế UML chỉnh sửa phần xử lý error 404

---
+++ src/main/java/com/itproject/controller/Error404.java
@@ -0,0 +1,18 @@
+package com.itproject.controller;
+
+import com.itproject.variable.RedirectPage;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = {RedirectPage.ERROR404_SERVLET})
+public class Error404 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher(RedirectPage.ERROR_404_PAGE).forward(req, resp);
+    }
+}
